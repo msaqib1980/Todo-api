1c792ff22bfd9379dce08131c17042eeb37d6944
Muhammad Saqib <saqib@cellsmartpos.com>
2016-10-02 19:46:09 -0400

    change to object

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [{
     id: 1,
-    description: 'Meet mom for lunch',
+    description: 'Meet Ben for lunch',
     completed: false
 }, {
     id: 2,

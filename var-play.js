4704511e1a1116a5987f7051a1e604a5e9ae55ec
Muhammad Saqib <saqib@cellsmartpos.com>
2016-10-02 22:13:59 -0400

    add new test file

---
+++ var-play.js
@@ -0,0 +1,31 @@
+//var person = {
+//name:'saqib',
+//age:21
+//};
+//
+//
+//function updatePerson(obj){
+////    obj = {
+////        name: 'saqib',
+////        age:24
+////    };
+//    
+//    obj.age = 24;
+//}
+//
+//updatePerson(person);
+//
+//console.log(person);
+//
+
+var grades = [15,30];
+
+
+function addInToArray(gradesArr){
+    gradesArr.push(55);
+    debugger;
+//    gradesArr = [45,65,56];
+}
+
+addInToArray(grades);
+console.log(grades);

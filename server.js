d013bde94e0e87e7bfb00337bdf3eede182c0f73
Muhammad Saqib <saqib@cellsmartpos.com>
2016-10-06 16:59:55 -0400

    put working now

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/todos/:id',function(req,res){
         }
     },function(e){
         res.status(500).send(e);
-    })
+    });
     
 //    var matchedTodo = _.findWhere(todos, {id: todoId});
     
@@ -111,48 +111,72 @@ app.post('/todos',function(req,res){
 //DELETE /todos/:id
 app.delete('/todos/:id',function(req,res){
     var todoId = parseInt(req.params.id,10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
     
-    if (!matchedTodo) {
-        res.status(404).json({"error":"no todo found with that id"});
-    }else{
-        todos = _.without(todos,matchedTodo);
-        res.json(matchedTodo);
-    }
+    db.todo.destroy({
+        where:{
+            id: todoId
+        }
+    }).then(function(rewsDeleted){
+        if(rewsDeleted === 0 ){
+            res.status(404).json({
+                error: 'no todo with id'
+            });
+        }else{
+            res.status(204).send();
+        }
+    },function(){
+       res.status(404).send(); 
+    });
+    
+//    var matchedTodo = _.findWhere(todos, {id: todoId});
+//    
+//    if (!matchedTodo) {
+//        res.status(404).json({"error":"no todo found with that id"});
+//    }else{
+//        todos = _.without(todos,matchedTodo);
+//        res.json(matchedTodo);
+//    }
 });
 
 //PUT /todos/:id
 app.put('/todos/:id',function(req,res){
     var todoId = parseInt(req.params.id,10);
     
-    
-    var matchedTodo = _.findWhere(todos, {id: todoId});
+//    var matchedTodo = _.findWhere(todos, {id: todoId});
     
     var body = _.pick(req.body,'description','completed');
-    var validAttributes = {};
+    var attributes = {};
     
-    if (!matchedTodo){
-        return res.status(404).send('id did not found');
+//    if (!matchedTodo){
+//        return res.status(404).send('id did not found');
+//    }
+    
+    
+    
+    if (body.hasOwnProperty('completed')){
+        attributes.completed = body.completed;
     }
     
-    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
-        validAttributes.completed = body.completed;
-    }else if(body.hasOwnProperty('completed')){
-        return res.status(400).send();
+     if (body.hasOwnProperty('description')){
+        attributes.description = body.description;
     }
     
-     if (body.hasOwnProperty('description') && !_.isString(body.description) && body.description.trim().length === 0){
-        validAttributes.description = body.description;
-    }else if(body.hasOwnProperty('description')){
-        return res.status(400).send();
-    }
+    db.todo.findById(todoId).then(function(todo){
+        if (todo){
+            todo.update(attributes).then(function(todo){
+                    res.json(todo.toJSON());
+                },function(e){
+                    res.status(400).json(e);
+            });
+        }else{
+            res.status(404).send("id not found!");
+        }
+    },function(e){
+        res.status(500).send(e);
+    });
     
-    console.log(validAttributes);
-    
-  
-    
-    _.extend(matchedTodo,validAttributes);
-    res.json(matchedTodo);
+//    _.extend(matchedTodo,attributes);
+//    res.json(matchedTodo);
     
 });
 

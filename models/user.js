474feaceac6eab0e084fc0103bcc1e8ce9b60e53
Muhammad Saqib <saqib@cellsmartpos.com>
2016-10-07 16:37:25 -0400

    add new route

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
 module.exports = function(sequelize, DataTypes) {
-      return sequelize.define('user', {
+      var user = sequelize.define('user', {
           email:{
               type:DataTypes.STRING,
               allowNull: false,
@@ -8,11 +11,25 @@ module.exports = function(sequelize, DataTypes) {
                   isEmail:true
               }
           },
+          salt:{
+            type: DataTypes.STRING  
+          },
+          password_hash:{
+            type: DataTypes.STRING  
+          },
           password: {
-              type: DataTypes.STRING,
+              type: DataTypes.VIRTUAL,
               allowNull: false,
               validate: {
                   len: [7,100]
+              },
+              set: function(value){
+                  var salt = bcrypt.genSaltSync(10);
+                  var hashedPassword = bcrypt.hashSync(value,salt);
+                  
+                  this.setDataValue('password', value);
+                  this.setDataValue('salt',salt);
+                  this.setDataValue('password_hash',hashedPassword);
               }
           }
       },{
@@ -22,7 +39,39 @@ module.exports = function(sequelize, DataTypes) {
                       user.email = user.email.trim().toLowerCase();
                   }
               }
+          },
+          instanceMethods:{
+              toPublicJSON:function(){
+                  var json = this.toJSON();
+                  return _.pick(json,'id','email','createdAt','updatedAt');
+              }
+          },
+          classMethods: {
+              authenticate: function(){
+                  return new Promise(function(resolve, reject){
+                   if(typeof body.email !== 'string' || typeof body.password !== 'string'){
+                       return reject();
+                   }
+
+                    user.findOne({
+                        where:{
+                            email:body.email
+                        }
+                    }).then(function(user){
+                        if(!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+                            return reject();
+                        }
+
+                        resolve(user);
+                        
+                    },function(e) {
+                        reject();
+                    });
+                  });
+              }
           }
       });
+    
+    return user;
     
 };

95f1dd823a598a765d0a63f8d8b02e77243ee09f
Muhammad Saqib <saqib@cellsmartpos.com>
2016-10-08 07:53:29 -0400

    Adding DELETE /users/login

--- server.js
+++ server.js
@@ -159,16 +159,19 @@ app.post('/users',function(req,res){
 });
 
 app.post('/users/login',function(req,res){
-   var body = _.pick(req.body,'email','password');
+    var body = _.pick(req.body,'email','password');
+    var userInstance;
     
     db.user.authenticate(body).then(function(user){
         var token = user.generateToken('authenticate')
-        if(token){
-            res.header('Auth', token).json(user.toPublicJSON());
-        }else{
-            res.status(401).send();
-        }
-    },function(e){
+          userInstance = user;
+          return db.token.create({
+              token:token
+          });
+        
+    }).then(function(tokenInstance){
+        res.header('Auth',tokenInstance.get('token')).json(userInstance.toPublicJSON());  
+    }).catch(function(e){
         res.status(401).send();
     });
     
@@ -185,9 +188,20 @@ app.get('/users',function(req,res){
          res.status(500).send();
    });
 });
+
+//DELETE /users/login
+app.delete('/users/login', middleware.requireAuthentication,function(req,res){
+    req.token.destroy().then(function(){
+        res.status(204).send();
+    }).catch(function(e){
+        res.status(500).send(e);
+    });
+});
    
 
-db.sequelize.sync({force:true}).then(function(){
+db.sequelize.sync(
+    {force:true}
+).then(function(){
         app.listen(PORT,function(){
         console.log('Express listing on port ' + PORT + '!');
         });
